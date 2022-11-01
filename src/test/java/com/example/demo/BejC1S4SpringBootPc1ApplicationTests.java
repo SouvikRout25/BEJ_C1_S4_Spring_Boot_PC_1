fed70e65fbd11eb8bc339544698f19f9a65e75b2
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-11-01 15:10:44 +0530

    Initial commit

---
+++ src/test/java/com/example/demo/BejC1S4SpringBootPc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC1S4SpringBootPc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
